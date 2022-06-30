7ed153aa14a94af66904a77f625042fc1384496c
Hamjaster <87142882+Hamjaster@users.noreply.github.com>
2022-06-30 19:09:46 +0500

    Add files via upload

---
+++ Gb.js
@@ -0,0 +1,141 @@
+import React from 'react'
+
+export default function Gb() {
+    console.log('you are in me')
+  return (
+    <>
+        <div className="jumbotron">
+          <h1 className="display-5">The Alternative version of Whatsapp you should try out <img width="70px" src="gb2.png" className="img-fluid" alt=""/> </h1>
+          <p className="lead
+          ">
+           Tired of the simple features of Whatsapp? Now try out the 
+           GBWhatsapp, rich in handy and useful features.
+
+          </p>
+          <hr className="my-4" />
+          <p className=" main">
+
+            These days the technology and internet has diminished the 
+            human efforts to an unimaginalbe extinct for example
+            Messaging apps play a vital role in our daily life. You must
+            be using Whatsapp which is the most popular messenger and
+            due to it's incredible features and user experience. 
+           
+            
+            <div className="head">What is GBWhatsapp</div>
+                GBWhatsapp is basically the moded version of Whatsapp.
+                It is created to enlarge the features of Whatsapp. You 
+                will not find this modified version of Whatsapp on official
+                Playstore, But you can easily get this either from this page 
+                or from the Google Chrome. <br/><p><div className="list">Here is the list of some 
+               popular features of GBWhatspp :</div></p> 
+               <ul>
+                 <li>Improved privacy options</li>
+                 <li>Option to hide last viewed</li>
+                 <li>Option to hide read receipts</li>
+                 <li>Option to anti-delete other's status</li>
+                 <li>Option to hide last seen for specific contacts</li>
+                 <li>Uses a group name of up to 35 characters</li>
+                 <li>Option to hide message delivered</li>
+                 <li>Distinguish between normal messages and bulk messages</li>
+                 <li> Establish more than three chats</li>
+                 <li>Send broadcasts to 600 people, rather than 250 people</li>
+                 <li>Use a status with up to 255 characters as opposed to the traditional 139 characters</li>
+               </ul>
+              
+               <div className="head">Download GBWhatsapp</div>
+               <p>Due to it's satisfactory qualities and features downloading 
+                 this will be a useful step for you. You can download 
+                 GBWhatsapp either from official website at <a href="https://gbapps.net/download-gbwhatsapp-apk/">gbapps.net</a>
+                 or easily from this download button, directly downloading from here
+                 saves you from ads and multiple downloading options which may confuse you in official website.
+
+                 <br/><a href="GBwhatsapp_Sep.apk" download="GBWhatspp.apk"><button className="btn btn-lg btn-primary"><i className="fa fa-download"></i> Download</button></a>
+               </p>
+               
+          </p>
+        </div>
+      
+<footer className="text-center text-lg-start bg-light text-muted">
+
+  <section
+    className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+   
+    <div className="me-5 d-none d-lg-block">
+      <span>Get connected with me on social networks:</span>
+    </div>
+  
+
+
+    <div>
+      <a target="_" href="" className="me-4 text-reset">
+        <i className="fa fa-facebook-f"></i>
+      </a>
+      <a target="_" href="https://twitter.com/dev_hamzashah" className="me-4 text-reset">
+        <i className="fa fa-twitter"></i>
+      </a>
+    
+      <a target="_" href="https://www.instagram.com/dev_hamza_777/" className="me-4 text-reset">
+        <i className="fa fa-instagram"></i>
+      </a>
+      <a target="_" href="https://www.youtube.com/channel/UC5taoBtckhl4Y8z_wwWX0QA" className="me-4 text-reset">
+        <i className="fa fa-youtube"></i>
+      </a>
+      <a target="_" href="https://github.com/Hamjaster" className="me-4 text-reset">
+        <i className="fa fa-github"></i>
+      </a>
+    </div>
+ 
+  </section>
+
+
+
+  <section className="">
+    <div className="container text-center text-md-start mt-5">
+ 
+      <div className="row mt-3">
+   
+        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+      
+          <h6 className="text-uppercase fw-bold mb-4">
+            About Author
+          </h6>
+          <p>
+            Hamza here, A boy obessed in programming, blogging, 
+            web development and ETC...
+          </p>
+        </div>
+        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+ 
+          <h6 className="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i className="fa fa-home" aria-hidden="true"></i> Wah Cantt, Pakistan</p>
+          <p>
+        
+            hamzashah.dev@gmail.com
+          </p>
+          <p><i className="fa fa-phone me-3"></i> + 01 234 567 88</p>
+        <p><i className="fa fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+      
+      </div>
+     
+    </div>
+  </section>
+
+
+
+  <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
+    © 2021 Copyright:
+    <a className="text-reset fw-bold" href="https://hamjaster.github.io/Hamza-Shah/" target="_">DEV Hamza</a>
+  </div>
+
+</footer>
+
+        
+
+</>
+  )
+}
